556ef98ee7759ae74b1acf7fb37f383c3402ca9b
RassuK <RassuK@users.noreply.github.com>
2018-05-24 18:50:41 +0300

    Added class for CSV parsing logic

---
+++ classes/ParseCSV.class.php
@@ -0,0 +1,110 @@
+<?php
+    class CSV{
+
+        function __construct($CSV){
+            $this->_CSV = $CSV;
+            $this->file = fopen($CSV,"r");
+        }
+
+        public function getMonthlyValues($year){
+            $monthlyValues=array(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+            $months=array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
+            fseek($this->file, 0);
+            while(! feof($this->file))
+            {
+            $array = fgetcsv($this->file, 0, ";");
+            $month = substr($array[0], -13, 2);
+            $yearValue = substr($array[0], -10, 4);
+                if($year == $yearValue){
+                    foreach($months as $key=>$value){
+                        if($month == $value){
+                            $monthlyValues[$key] += floatval(preg_replace("/[^-0-9\.]/",".",$array[2]));
+                        }
+                    }
+                }
+            
+            }
+            
+            return $monthlyValues;
+
+        }
+
+        public function getYearlyValues(){
+            $yearValues = array();
+            $years = array();
+            $yearlyValues = array();
+            fseek($this->file, 0);
+            while(! feof($this->file)){
+                $array = fgetcsv($this->file, 0, ";");
+                $yearValue = substr($array[0], -10, 4);
+                if(!in_array($yearValue, $years)){
+                    array_push($years, $yearValue);
+                }
+            }
+            $years = array_filter($years); //Workaround for 1 empty key in the end
+            fseek($this->file, 0);
+            while(! feof($this->file)){
+                $array = fgetcsv($this->file, 0, ";");
+                foreach($years as $key=>$value){
+                    if(substr($array[0], -10, 4) == $value){ 
+                        if(!isset($yearlyValues[$key])) { //Workaround for undefined offset notice
+                            $yearlyValues[$key] = 0;
+                        }
+                        $yearlyValues[$key] += floatval(preg_replace("/[^-0-9\.]/",".",$array[2]));
+                    }
+                }
+            }
+            array_push($yearValues, $yearlyValues);
+            array_push($yearValues, $years);
+            return $yearValues ;   
+        }
+
+        public function getWeeklyValues(){
+            $weekDays = array(1, 2, 3, 4, 5, 6, 7);
+            $dailyValues = array(0, 0, 0, 0, 0, 0, 0);
+            $dayCount = array(0, 0, 0, 0, 0, 0, 0);
+            $average = array(0, 0, 0, 0, 0, 0, 0);
+            fseek($this->file, 0);
+            while(! feof($this->file)){
+                $array = fgetcsv($this->file, 0, ";");
+                $date = (substr($array[0], 0, 10));
+                //echo $date;
+                $weekDay = date("N", strtotime($date));
+                foreach($weekDays as $key=>$day){
+                    if($weekDay == $day){
+                        $dailyValues[$key] += floatval(preg_replace("/[^-0-9\.]/",".",$array[2]));
+                        $dayCount[$key] += 1;
+                    }
+                }
+
+            }
+            foreach($dayCount as $key=>$count){
+                $average[$key] = $dailyValues[$key]/$count;
+            }
+            return $average;
+        }
+
+        public function getDailyValues(){
+            $times = array('00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23');
+            $hourlyValues = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            $hourCount = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            $average = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            fseek($this->file, 0);
+            while(! feof($this->file)){
+                $array = fgetcsv($this->file, 0, ";");
+                $time = (substr($array[0], 11, 2));
+                foreach($times as $key=>$hour){
+                    if($time == $hour){
+                        $hourlyValues[$key] += floatval(preg_replace("/[^-0-9\.]/",".",$array[2]));
+                        $hourCount[$key] += 1;
+                    }
+                }
+
+            }
+            foreach($hourCount as $key=>$count){
+                $average[$key] = $hourlyValues[$key]/$count;
+            }
+            return $average;
+        }
+
+    }
--- raport.php
+++ raport.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	require("config.php");
+    require("config.php");
 
 	if(!isset($_SESSION["user_id"])){
 		header("Location: index.php");
@@ -18,33 +18,18 @@
     createNavbar();
     createNewRaportModal();
     
+    require_once("classes/ParseCSV.class.php");
+    $csvClass = new CSV("csv/tarbimisteatis.csv");
+    $list = $csvClass->getMonthlyValues("2017");
+    $list2 = $csvClass->getYearlyValues();
+    $list3 = $csvClass->getWeeklyValues();
+    $list4 = $csvClass->getDailyValues();
 ?>
 <div class="container">
-  <ul class="nav nav-tabs">
-    <li class="active"><a data-toggle="tab" href="#Raport1">Raport 1</a></li>
-    <li><a data-toggle="tab" href="#Raport2">Raport 2</a></li>
-    <li><a data-toggle="tab" href="#Raport3">Raport 3</a></li>
-    <li><a data-toggle="tab" href="#Raport4">Raport 4</a></li>
-    <button type="button" class="btn btn-primary btn-md" style="margin-top:4px" data-toggle="modal" data-target="#newRaportModal">Create New</button>
-  </ul>
-
-  <div class="tab-content">
-    <div id="Raport1" class="tab-pane fade in active">
-      <div id="chartContainer" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-    </div>
-    <div id="Raport2" class="tab-pane fade">
-      <h3>Raport 2</h3>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    <div id="Raport3" class="tab-pane fade">
-      <h3>Raport 3</h3>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-    </div>
-    <div id="Raport4" class="tab-pane fade">
-      <h3>Raport 4</h3>
-      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-    </div>
-  </div>
+<div id="chartContainer1" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+<div id="chartContainer" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+<div id="chartContainer2" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+<div id="chartContainer3" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 </div>
 
 <script>
@@ -76,10 +61,172 @@ Highcharts.chart('chartContainer', {
     },
     series: [{
         name: 'Yours',
-        data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }, {
-        name: 'Optimal',
-        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        data: [
+                <?php foreach($list as $value){
+                    echo $value . ", ";
+                }?>
+              ]
+    }]
+});
+
+Highcharts.chart('chartContainer1', {
+    chart: {
+        zoomType: 'x'
+    },
+    title: {
+        text: 'Yearly Electricity Usage'
+    },
+    xAxis: {
+        categories: [                
+                <?php 
+                $valueArray = $list2[1];
+                foreach($valueArray as $value){
+                echo $value . ", ";
+                }?>
+                ]
+    },
+    yAxis: {
+        title: {
+            text: 'KW'
+        }
+    },
+    plotOptions: {
+        line: {
+            dataLabels: {
+                enabled: true
+            },
+            enableMouseTracking: false
+        },
+        area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+    },
+    series: [{
+        name: 'Yours',
+        type: 'column',
+        data:   [
+                <?php 
+                $valueArray = $list2[0];
+                foreach($valueArray as $value){
+                echo $value . ", ";
+                }?>
+                ]
+    }]
+});
+
+Highcharts.chart('chartContainer2', {
+    chart: {
+        zoomType: 'x'
+    },
+    title: {
+        text: 'Average Electricity Usage on Weekdays'
+    },
+    xAxis: {
+        categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    },
+    yAxis: {
+        title: {
+            text: 'KW'
+        }
+    },
+    plotOptions: {
+        line: {
+            dataLabels: {
+                enabled: true
+            },
+            enableMouseTracking: false
+        },
+        area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+    },
+    series: [{
+        name: 'Yours',
+        type: 'column',
+        data:   [
+                <?php 
+                foreach($list3 as $value){
+                echo $value . ", ";
+                }?>
+                ]
+    }]
+});
+
+Highcharts.chart('chartContainer3', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Electricity Usage by Time of Day'
+    },
+    subtitle: {
+        text: 'Source: You'
+    },
+    xAxis: {
+        categories: ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+    },
+    yAxis: {
+        title: {
+            text: 'KW'
+        }
+    },
+    plotOptions: {
+        line: {
+            dataLabels: {
+                enabled: true
+            },
+            enableMouseTracking: false
+        }
+    },
+    series: [{
+        name: 'Yours',
+        data: [
+                <?php foreach($list4 as $value){
+                    echo $value . ", ";
+                }?>
+              ]
     }]
 });
 </script>
