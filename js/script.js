74f91aae434a7fbe3ab89fb1f4a0cbfbee9ee147
RassuK <RassuK@users.noreply.github.com>
2018-05-25 21:57:47 +0300

    Set datepicker's first day to monday

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 
     $( function() {
-        $( "#datepicker" ).datepicker({ dateFormat: 'dd.mm.yy' });
+        $( "#datepicker" ).datepicker({ dateFormat: 'dd.mm.yy', firstDay: 1 });
     });
 
 });
